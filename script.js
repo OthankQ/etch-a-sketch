2fc5b899bf69a89cd63307833ca118cb180444fb
Ryan <ryank9462@gmail.com>
2022-02-25 22:52:22 -0600

    Add mouseenter eventlistener to cells and split out functions.

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 function createGrid(){
-    let gridSize = prompt('How many grid?');
+    let gridSize = getGridSize();
+
     const grid = document.querySelector('.container');
 
     grid.style.gridTemplateRows = `repeat(${gridSize}, auto)`
     grid.style.gridTemplateColumns = `repeat(${gridSize}, auto)`
 
+    addCells(gridSize, grid);
+}
 
-    for (let i = 0; i < gridSize**2; i ++){
+function addCells(size, parentElement) {
+    for (let i = 0; i < size**2; i ++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
-        grid.appendChild(cell);
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = `black`;
+        })
+        parentElement.appendChild(cell);
     }
+}
+
+function getGridSize() {
+    let gridSize;
+    // Limit number of cells
+    do {
+        gridSize = prompt('How many grid?');
+    } while (gridSize > 100);
+
+    if (gridSize === null) {
+        return;
+    }
+
+    return gridSize;
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random * 255) + 1;
 }
 
 createGrid();
