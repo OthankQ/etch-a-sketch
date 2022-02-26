762c2fd518377a455c210f609c2051375683f326
Ryan <ryank9462@gmail.com>
2022-02-26 10:22:57 -0600

    Add reset button and functionality

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
+const resetBtn = document.querySelector('.resetBtn');
+resetBtn.addEventListener('click', deleteCells);
+
 function createGrid(){
     let gridSize = getGridSize();
 
-    const grid = document.querySelector('.container');
+    const grid = document.querySelector('.grid');
 
     grid.style.gridTemplateRows = `repeat(${gridSize}, auto)`
     grid.style.gridTemplateColumns = `repeat(${gridSize}, auto)`
 
-    addCells(gridSize, grid);
+    createCells(gridSize, grid);
 }
 
-function addCells(size, parentElement) {
+function createCells(size, parentElement) {
     for (let i = 0; i < size**2; i ++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
@@ -20,18 +23,24 @@ function addCells(size, parentElement) {
     }
 }
 
+function deleteCells() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => {
+        cell.remove()
+    })
+    createGrid();
+}
+
 function getGridSize() {
     let gridSize;
     // Limit number of cells
     console.log(typeof gridSize);
-    while (gridSize > 100 || gridSize < 1 ||gridSize === undefined || gridSize === NaN) {
-        gridSize = prompt('How many grid?');
+    while (gridSize > 100 || gridSize < 1 || gridSize === undefined || gridSize === NaN) {
+        gridSize = prompt('How many grid? (Enter a number between 1 and 100)');
     };
-
     if (gridSize === null) {
         return;
     }
-
     return gridSize;
 }
 
